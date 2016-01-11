56591669cf4ee539f33b46cec8deee62a02ff721
jogi32 <tomasz.szafranski32@gmail.com>
2016-01-11 22:19:06 +0100

    Main WIndow value control added

--- WeatherStation_WPF/MainWindow.xaml.cs
+++ WeatherStation_WPF/MainWindow.xaml.cs
@@ -31,11 +31,33 @@ namespace WeatherStation_WPF
             DataChart = new DataChartWindow();
             DataGraph = new DataGraphWindow();
 
-            //DataGraph.Show();
-            //DataChart.Show();
+            DataGraph.Show();
+            DataChart.Show();
 
             serialController = new SerialPortController();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 1);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.IsEnabled = true;
         }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            ProgBarT1.Value         = DataHolder.i_temperature1;
+            T1Value.Content         = DataHolder.i_temperature1.ToString();
+            ProgBarT2.Value         = DataHolder.i_temperature2;
+            T2Value.Content         = DataHolder.i_temperature2.ToString();
+            ProgBarT3.Value         = DataHolder.i_temperature3;
+            T3Value.Content         = DataHolder.i_temperature3.ToString();
+            ProgBarPressure.Value   = DataHolder.i_pressureCalculated;
+            PressureValue.Content   = DataHolder.i_pressureCalculated.ToString();
+            ProgBarHeight.Value     = DataHolder.i_heightCalculated;
+            HeightValue.Content     = DataHolder.i_heightCalculated;
+            pbStatus.Value          = DataHolder.i_radianceCalculated / 10;
+            RadianceValue.Content   = DataHolder.i_radianceCalculated.ToString();
+        }
+            
 
         private void But_Bt_Stop_Click(object sender, RoutedEventArgs e)
         {
